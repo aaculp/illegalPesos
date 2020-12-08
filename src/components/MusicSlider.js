61a3562860b4647aff2c9d15d78525872a9a3b41
Aaron <a-a-ron@Aarons-iMac.attlocal.net>
2020-12-07 21:19:54 -0500

    didnt add much

--- src/components/MusicSlider.js
+++ src/components/MusicSlider.js
@@ -1,21 +1,6 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 
-const styles = {
-  color: "red",
-  textDecoration: "none",
-};
-
-const divStyle = {
-  fontSize: "15px",
-  color: "ccc",
-  overflow: "hidden",
-  textOverflow: "ellipsis",
-  fontFamily:
-    "Interstate, Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif",
-  fontWeight: "100",
-};
-
 export default function MusicSlider() {
   return (
     <div style={{ textAlign: "center" }}>
@@ -23,99 +8,99 @@ export default function MusicSlider() {
       <Carousel>
         <Carousel.Item>
           <iframe
+            title="WTM Scoob World Tour Mafia Mix"
             width="50%"
             height="150px"
             scrolling="no"
-            frameborder="yes"
-            allow="autoplay"
+            frameBorder="no"
             pause="hover"
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/827198962&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
           ></iframe>
         </Carousel.Item>
         <Carousel.Item>
           <iframe
+            title="Detour Outwest Mix"
             width="50%"
             height="150px"
             scrolling="no"
-            frameborder="no"
-            allow="autoplay"
+            frameBorder="no"
             pause="hover"
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/780050401&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
           ></iframe>
         </Carousel.Item>
         <Carousel.Item>
           <iframe
+            title="Toosii Platinum Heart Mix"
             width="50%"
             height="150px"
             scrolling="no"
-            frameborder="no"
-            allow="autoplay"
+            frameBorder="no"
             pause="hover"
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/779419846&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
           ></iframe>
         </Carousel.Item>
         <Carousel.Item>
           <iframe
+            title="Big Speaker Mix 2020"
             width="50%"
             height="150px"
             scrolling="no"
-            frameborder="no"
-            allow="autoplay"
+            frameBorder="no"
             pause="hover"
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/743482729&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
           ></iframe>
         </Carousel.Item>
         <Carousel.Item>
           <iframe
+            title="Chicago Mix Volume 3"
             width="50%"
             height="150px"
             scrolling="no"
-            frameborder="no"
-            allow="autoplay"
+            frameBorder="no"
             pause="hover"
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/656501687&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
           ></iframe>
         </Carousel.Item>
         <Carousel.Item>
           <iframe
+            title="Chicago Mix Volume 2"
             width="50%"
             height="150px"
             scrolling="no"
-            frameborder="no"
-            allow="autoplay"
+            frameBorder="no"
             pause="hover"
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/567255762&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
           ></iframe>
         </Carousel.Item>
         <Carousel.Item>
           <iframe
+            title="Chicago Mix"
             width="50%"
             height="150px"
             scrolling="no"
-            frameborder="no"
-            allow="autoplay"
+            frameBorder="no"
             pause="hover"
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/511906158&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
           ></iframe>
         </Carousel.Item>
         <Carousel.Item>
           <iframe
+            title="Illegal Summer Mix"
             width="50%"
             height="150px"
             scrolling="no"
-            frameborder="no"
-            allow="autoplay"
+            frameBorder="no"
             pause="hover"
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/490168581&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
           ></iframe>
         </Carousel.Item>
         <Carousel.Item>
           <iframe
+            title="Str8 Hits Never Miss Pesos"
             width="50%"
             height="150px"
             scrolling="no"
-            frameborder="no"
-            allow="autoplay"
+            frameBorder="no"
             pause="hover"
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/410314287&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
           ></iframe>
